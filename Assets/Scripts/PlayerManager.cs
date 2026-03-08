eed9a5cb617e153bf5dbdd30b673f0cc5e7aaeeb
hollloow <pedroalaminomat@gmail.com>
2026-03-08 18:03:12 -0300

    Commit 2
    controles feitos com o novo input sistem
    
    preciso melhorar o pulo

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,41 +1,32 @@
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerManager : MonoBehaviour
 {
-    [SerializeField] float moveSpeed;
-    private Rigidbody2D rb;
-
-    private InputControl inputControls;
-    private Vector2 inputDirector => inputControls.Player.Move.ReadValue<Vector2>();
-    Vector2 move;
+    [SerializeField] int moveSpeed;
+    Vector2 movement;
+    Rigidbody2D rb;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        inputControls = new InputControl();
-        inputControls.Enable();
+    }
+
+    public void SetMovement (InputAction.CallbackContext value)
+    {
+        movement = value.ReadValue<Vector2>();
+    }
+    public void SetJump (InputAction.CallbackContext value)
+    {
+        rb.AddForce(Vector2.up * 100);
     }
 
     private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(moveSpeed * Time.deltaTime * move.x, rb.linearVelocity.y);
+        rb.linearVelocity = new Vector2(moveSpeed * Time.deltaTime * movement.x, rb.linearVelocity.y);
     }
 
-    private void Update()
-    { 
-        move = new(inputDirector.x,0);
-        
-    }
-
-    private void OnDestroy()
-    {
-        inputControls.Disable();
-    }
-
-    private void OnDisable()
-    {
-        inputControls.Disable();
-    }
 }
+
